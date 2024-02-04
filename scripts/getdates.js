874840d156dfe113305f145ccb0ab6b94623431e
MitzukiChan1 <Mario.70887611@gmail.com>
2024-02-03 18:50:18 -0600

    w4

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,15 +1,17 @@
-
 function setCurrentYear() {
     const year = new Date().getFullYear();
     document.getElementById('currentYear').textContent = year;
 }
-
 
 function setLastModifiedDate() {
     const lastModified = document.lastModified;
     document.getElementById('lastModified').textContent = 'Last Updated: ' + lastModified;
 }
 
+function toggleMenu() {
+    var nav = document.querySelector('nav');
+    nav.classList.toggle('show-menu');
+}
 
 setCurrentYear();
 setLastModifiedDate();
